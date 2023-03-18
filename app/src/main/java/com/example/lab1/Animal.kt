5e0b1213ec812eaa1e434482fa6610ea5cbd3a51
maks <wether.ryia@gmail.com>
2023-03-18 18:25:56 +0200

    1

---
+++ app/src/main/java/com/example/lab1/Animal.kt
@@ -0,0 +1,9 @@
+package com.example.lab1
+
+import android.graphics.drawable.Drawable
+import android.provider.MediaStore.Images
+
+data class Animal(val name:String,var age:Int,var description:String
+) {
+
+}
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text1=findViewById<TextView>(R.id.textView2)
+        val text2=findViewById<TextView>(R.id.textView3)
+        val text3=findViewById<TextView>(R.id.textView4)
+        val button1=findViewById<Button>(R.id.button)
+        val button2=findViewById<Button>(R.id.button3)
+        val button3=findViewById<Button>(R.id.button2)
+
+
+        val image1=resources.getIdentifier("cat1","drawable",packageName)
+        val image2=resources.getIdentifier("cat3","drawable",packageName)
+        val image3=resources.getIdentifier("cat2","drawable",packageName)
+        var cat1=Animal("Мурзик",5,"Сіресенький грайливий котик")
+        var cat2=Animal("Пончик",5,"Сіренький повненький котик")
+        var cat3=Animal("Пако",5,"Білесенький котик")
+        text1.text=cat1.name;
+        text2.text=cat3.name;
+        text3.text=cat2.name;
+        button1.setOnClickListener {
+            goleyaut(image1,button1,cat1)
+        }
+        button2.setOnClickListener {
+            goleyaut(image2,button2,cat2)
+        }
+        button3.setOnClickListener {
+            goleyaut(image3,button3,cat3)
+        }
+
+
+
+
+
+    }
+    fun goleyaut(image:Int, button: Button, cat:Animal){
+        setContentView(R.layout.deteil)
+        val image1 = findViewById<ImageView>(R.id.imageView4)
+        val nameText = findViewById<TextView>(R.id.textView)
+        val descriptionText = findViewById<TextView>(R.id.textView5)
+        nameText.text=cat.name
+        descriptionText.text=cat.description
+        image1.setImageResource(image)
     }
 }
